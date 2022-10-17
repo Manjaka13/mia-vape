b50409cfaafaa06eac9349532c70a6c87f6aafa9
harijaona <harijaona@expat.com>
2022-10-17 17:39:32 +0300

    update styls

--- components/GroupBuyPresentation.jsx
+++ components/GroupBuyPresentation.jsx
@@ -10,33 +10,35 @@ import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
 const GroupBuyPresentation = () => (
 	<div className="group-buy-presentation">
 		<div className="content-box">
-			<div className="left">
-				<h2 className="title">Group Buy</h2>
-				<div className="icon">
-					<Icon icon={faPeopleGroup} />
+			<div className="content">
+				<div className="left">
+					<h2 className="title">Group Buy</h2>
+					<div className="icon">
+						<Icon icon={faPeopleGroup} />
+					</div>
 				</div>
-			</div>
-			<div className="right">
-				<p className="section__text">
-					Le group buy est un concept où vous et votre groupe convenez à{" "}
-					<strong>acheter</strong> par exemple une cigarette électronique sur le site
-					Mia Vape en groupe. Il s&apos;agit d&apos;un très bon moyen pour casser les
-					prix et profiter d&apos;un large réduction sur un article sous réserve que
-					vous achetiez tous en même temps par groupe de 5 ou 10. Vous pouvez
-					toutefois aussi profiter des{" "}
-					<Link href="#promotions">
-						<a className="section__link" title="Voir">
-							promotions
+				<div className="right">
+					<p className="section__text">
+						Le group buy est un concept où vous et votre groupe convenez à{" "}
+						<strong>acheter</strong> par exemple une cigarette électronique sur le
+						site Mia Vape en groupe. Il s&apos;agit d&apos;un très bon moyen pour
+						casser les prix et profiter d&apos;un large réduction sur un article sous
+						réserve que vous achetiez tous en même temps par groupe de 5 ou 10. Vous
+						pouvez toutefois aussi profiter des{" "}
+						<Link href="#promotions">
+							<a className="section__link" title="Voir">
+								promotions
+							</a>
+						</Link>{" "}
+						en cours ou directement initier une séance de <strong>group buy</strong>{" "}
+						sur un article dans notre catalogue.
+					</p>
+					<Link href="/group-buy">
+						<a className="button" title="Consulter">
+							En savoir plus
 						</a>
-					</Link>{" "}
-					en cours ou directement initier une séance de <strong>group buy</strong>{" "}
-					sur un article dans notre catalogue.
-				</p>
-				<Link href="/group-buy">
-					<a className="button" title="Consulter">
-						En savoir plus
-					</a>
-				</Link>
+					</Link>
+				</div>
 			</div>
 		</div>
 	</div>
